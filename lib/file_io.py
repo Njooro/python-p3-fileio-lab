75bbf87e36d0778010f89d30928f66c49c27dc13
Maxwell <njoromax5@gmail.com>
2024-01-03 15:03:40 +0300

    test

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,32 @@
 def write_file(file_name, file_content):
-    pass
+    
+    """
+    Write content to a file.
+    Args:
+        file_name (str): The name of the file.
+        file_content (str): The content to write to the file.
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_to_file(file_name, append_content):
+    """
+    Append content to a file.
+    Args:
+        file_name (str): The name of the file.
+        append_content (str): The content to append to the file.
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write("\n" + append_content)
 
 def read_file(file_name):
-    pass
+    """
+    Read and return the content of a file.
+    Args:
+        file_name (str): The name of the file.
+    Returns:
+        str: The content of the file.
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        content = file.read()
+    return content
